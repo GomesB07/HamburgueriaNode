18f079be91d9fed9e871b2aaec9b9c7a86ddefe4
Bruno Luis <bruno.dev07@gmail.com>
2023-03-31 20:29:32 -0300

    language changes in constants

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.use(express.json())
 const port = 3000
 
 
-const preparo = 'Em preparação'
-const pronto = 'Pronto'
+const preparation = 'In preparation'
+const ready = 'Ready'
 const orders = []
 
 
@@ -34,17 +34,17 @@ app.get('/orders', methodAndPath, (request, response) => {
 })
 
 app.post('/order', methodAndPath, (request, response) => {
-    const {name, order, price} = request.body
-    const makeOrder = {id: uuid.v4(), name, order, price, status: preparo}
+    const {clientName, order, price} = request.body
+    const makeOrder = {id: uuid.v4(), clientName, order, price, status: preparation}
     orders.push(makeOrder)
     return response.status(201).json(makeOrder)
 })
 
 app.put('/order/:id', methodAndPath, checkOrderId, (request, response) => {
     const id = request.orderId
-    const {name, order, price} = request.body
+    const {clientName, order, price} = request.body
     const index = request.orderIndex
-    const udpatedOrder = {id, name, order, price, status: preparo}
+    const udpatedOrder = {id, clientName, order, price, status: preparation}
     orders[index] = udpatedOrder
 
     return response.status(200).json(udpatedOrder)
@@ -63,7 +63,7 @@ app.get('/order/:id', methodAndPath, checkOrderId, (request, response) => {
 
 app.patch('/order/:id', methodAndPath, checkOrderId, (request, response) => {
     const index = request.orderIndex
-    orders[index].status = pronto
+    orders[index].status = ready
     return response.json(orders[index])
 })
 
